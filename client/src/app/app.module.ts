366978832836b4976467813c9739796bf04d2378
andres620 <andres.1701625573@ucaldas.edu.co>
2019-07-21 11:50:38 -0500

    Creación de los componentes navbar, hero y footer.  Creación de los modelos desde el Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { HeroComponent } from './template/hero/hero.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
