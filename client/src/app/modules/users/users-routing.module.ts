401c865144a02c5398f598e6e4386482fae31509
andres620 <andres.1701625573@ucaldas.edu.co>
2019-08-06 13:40:04 -0500

    Creación de los componentes login, logout y register

---
+++ client/src/app/modules/users/users-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+
+const routes: Routes = [
+  {
+    path: 'users/register',
+    component: RegisterComponent
+  },
+  {
+    path: 'user/login',
+    component: LoginComponent
+  },
+  {
+    path: 'user/logout',
+    component: LogoutComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
---
+++ client/src/app/modules/users/users.module.ts
@@ -0,0 +1,22 @@
+ import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, LogoutComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ],
+  exports: [
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent
+  ]
+})
+export class UsersModule { }
