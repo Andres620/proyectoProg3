56efcaac1e15c8f9a3434a1b9d00f0ee0aaec240
andres620 <andres.1701625573@ucaldas.edu.co>
2019-08-13 14:34:46 -0500

    Creamos un metodo propio para obtener los articulos segun la id del autor

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(Article) {
 
+//REVISAR LUEGO
+module.exports = function (Article) {
+    Article.findArticleByAuthorID = function (authorId, cb) {
+        console.log('no me lo puedo creer: ',authorId)
+        console.log('no me lo puedo creer x2: ',authorId)
+        Article.find({
+            where: {
+                authorId: {
+                    gte: authorId
+                }
+            }
+        }, cb);
+    };
+    
+
+    Article.remoteMethod("findArticleByAuthorID", {
+        accepts: {
+            arg: "authorId",
+            type: "string",
+            required: true
+        },
+        returns: {
+            arg: "articles",
+            type: "array"
+        },
+        http: {
+            path: "/get-article-by-equals-authorId",
+            verb: "get"
+        }
+    })
 };
